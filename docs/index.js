1fc92aa9fe870968a9f7eea3e29ed5383545f295
Ben Uthoff <81537940+benuthoff@users.noreply.github.com>
2022-09-04 20:09:39 -0400

    Soundbox v0.2
    - Fixed Title from "Soundbar" to "Soundbox"
    - Added Among Us SFX
    - Fixed Blur Effect on Safari
    - Fixed SFX Timing

--- docs/index.js
+++ docs/index.js
@@ -4,11 +4,11 @@
 
 const sfx = [
 	'./sound_files/vine_boom.mp3',
-	'./sound_files/bruh.mp3'
+	'./sound_files/bruh.mp3',
+	'./sound_files/among_us.mp3'
 ]
 
 function play_index(ind) {
 	let aud = new Audio(sfx[ind]);
 	aud.play();
 }
-
