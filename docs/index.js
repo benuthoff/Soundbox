b9a65573661619424a859fa9a5474b874c30ec24
Ben Uthoff <81537940+benuthoff@users.noreply.github.com>
2022-09-04 20:43:56 -0400

    Soundbox v0.4
    - Added AHHH SFX
    - Fixed Colors

--- docs/index.js
+++ docs/index.js
@@ -5,7 +5,8 @@
 const sfx = [
 	'./sound_files/vine_boom.mp3',
 	'./sound_files/bruh.mp3',
-	'./sound_files/among_us.mp3'
+	'./sound_files/among_us.mp3',
+	'./sound_files/ahhh.mp3'
 ]
 
 function play_index(ind) {
